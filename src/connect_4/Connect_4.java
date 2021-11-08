f35da7efa99324ef7c71e3940b309ff07adc8415
Xavier D'Mello <18093763+Tacocat4642@users.noreply.github.com>
2021-11-08 14:21:45 -0500

    updated printBoard(), created printWinStreaks(), started creating placeDisc

--- src/connect_4/Connect_4.java
+++ src/connect_4/Connect_4.java
@@ -22,10 +22,16 @@ public class Connect_4
         int p2WinStreak = 0;
         int cpuWinStreak = 0;
         boolean playAgain = true;
+        boolean gameOver = false;
         String[][] board = new String[numRows][numColumns];
         Scanner myInput = new Scanner(System.in);
 
+        System.out.println("|| Connect 4 ||");
+
         while (playAgain = true) {
+
+            // Print win streaks
+            printWinStreaks(p1WinStreak, p2WinStreak, cpuWinStreak);
 
             // Get game mode choice
             gameMode = getGameMode();
@@ -33,24 +39,32 @@ public class Connect_4
             // Empty game board
             board = emptyBoard(board);
 
-            // Print game board
-            printBoard(board);
-
-
-        } // end game loop
+            // Main gameplay loop
+            while (gameOver == false) {
+                printBoard(board);
+                // Place disc for each player
+                board = placeDisc(board, gameMode, "X");
+                printBoard(board);
+                board = placeDisc(board, gameMode, "O");
+            }
+        } // end program loop
     } // end main()
     
     // Prints out Connect 4 board
     public static void printBoard(String[][] board)
     {
-        System.out.println("   || Connect 4 ||");
+        // Print title and column numbers
+        // TODO: Figure out how to centre title
+        System.out.println("|| Connect 4 ||");
+        for (int i = 0; i < board[0].length; i++) {
+            System.out.print(" " + (i+1) + " ");
+        }
+        System.out.println();
 
         // Print each row, starting from bottom left
-        for(int row = 0; row < board.length; row++ )
-        {
+        for (int row = 0; row < board.length; row++ ) {
             // Print each column in that row
-            for(int column = 0; column < board[row].length; column++)
-            {
+            for (int column = 0; column < board[row].length; column++) {
                 System.out.print("[" + board[row][column] + "]");
             }
             System.out.println();
@@ -61,11 +75,9 @@ public class Connect_4
     public static String[][] emptyBoard(String[][] board)
     {
         // Change each row, starting from bottom left
-        for(int row = 0; row < board.length; row++ )
-        {
+        for (int row = 0; row < board.length; row++ ) {
             // Change each column in that row
-            for(int column = 0; column < board[row].length; column++)
-            {
+            for (int column = 0; column < board[row].length; column++) {
                 board[row][column] = " ";
             }
         }
@@ -80,8 +92,7 @@ public class Connect_4
 
         while (true) {
             try {
-                System.out.println("   || Connect 4 ||");
-                System.out.println("Choose your game mode:\n1.Player vs. Player\n2.Player vs. CPU");
+                System.out.println("Choose your game mode:\n1: Player vs. Player\n2: Player vs. CPU");
                 gameMode = myInput.nextInt();
 
                 if (gameMode < 0 || gameMode > 1) {
@@ -96,4 +107,45 @@ public class Connect_4
         }
         return gameMode;
     } // end getGameMode()
-}
+
+    // Prints win streaks (if there are any)
+    public static void printWinStreaks(int p1WinStreak, int p2WinStreak, int cpuWinStreak)
+    {
+        if (p1WinStreak != 0) {
+            System.out.print("Player 1 Win Streak: " + p1WinStreak + " || ");
+
+            if (p2WinStreak != 0 || cpuWinStreak != 0) {
+                System.out.print(" | ");
+            }
+        }
+        if (p2WinStreak != 0) {
+            System.out.println("Player 2 Win Streak: " + p1WinStreak);
+
+            if (cpuWinStreak != 0) {
+                System.out.print(" | ");
+            }
+        }
+        if (cpuWinStreak != 0) {
+            System.out.print("CPU Win Streak: " + cpuWinStreak);
+        }
+    } // end printWinStreaks()
+
+    // Ask for choice and place disc in board
+    public static String[][] placeDisc(String[][] board, int gameMode, String turn)
+    {
+        Scanner myInput = new Scanner(System.in);
+        System.out.println("Player " + turn + ": Choose column to place disc in: ");
+        int placeDiscInColumn = myInput.nextInt() - 1;
+        int placeDiscAtRow = 0;
+
+        // Find what row in column to place chip at (making disc "fall to bottom")
+        for (int row = 0; row < board.length; row++) {
+            if (!board[row][placeDiscInColumn].equals(" ")) {
+                placeDiscAtRow = row + 1;
+            }
+        }
+
+        board[placeDiscAtRow][placeDiscInColumn] = turn;
+        return board;
+    } // end placeDisc()
+} // end class Connect_4
