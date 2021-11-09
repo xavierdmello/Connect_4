b8109f68576f67d87fd9fa9a79f107be24dec067
Xavier D'Mello <18093763+Tacocat4642@users.noreply.github.com>
2021-11-09 11:05:19 -0500

    Started working on AI (does not work at the moment)

---
+++ src/connect_4/Ai.java
@@ -0,0 +1,117 @@
+package connect_4;
+
+import java.util.Arrays;
+
+public class Ai
+{
+    public static String[][] makeMove(String[][] board, String turn)
+    {
+        int bestmove[] = checkForBestMove(board, turn);
+
+
+        return board;
+    }
+    public static int[] checkForBestMove(String[][] board, String turn)
+    {
+        int bestMove[] = {0, 0}; // Column with best move, score of that move
+        int highestScoreOfIteration[] = {0,0};
+        String[][] testBoard = board.clone();
+        int placeDiscAtRow = 999;
+
+        // Check each column's score
+        for (int placeDiscInColumn = 0; placeDiscInColumn < board[0].length; placeDiscInColumn++) {
+
+            // Find what row in column to place chip at (making disc "fall to bottom")
+            for (int row = 0; row < board.length; row++) {
+                if (!board[row][placeDiscInColumn].equals(" ")) {
+                    placeDiscAtRow = row + 1;
+                }
+            }
+
+            // If disc would overflow column, just move to the next one
+            if (placeDiscAtRow > 5) {
+                continue;
+            } else {
+
+                // See what the score is if you would put the disc in this column
+                testBoard[placeDiscAtRow][placeDiscInColumn] = turn;
+                highestScoreOfIteration = calculateHighestScore(testBoard, turn);
+
+                if (highestScoreOfIteration[1] > bestMove[1]) {
+                    bestMove[0] = highestScoreOfIteration[0];
+                    bestMove[1] = highestScoreOfIteration[0];
+                }
+            }
+        }
+
+        return bestMove;
+    } // end makeAiMove()
+
+    public static int[] calculateHighestScore(String[][] board, String turn)
+    {
+        // Go through each cell on the Connect 4 board
+        for (int row = 0; row < board.length; row++ ) {
+            for (int column = 0; column < board[row].length; column++) {
+
+                // Check for horizontal, vertical, left diagonal, and right diagonal 4-in-a-rows (respectively)
+                // This algo is probably super slow, but hey, it works and it's mine :)
+
+                try {
+                    // Check horizontals for wins
+                    int discsInARowSoFar = 0;
+                    for (int j = 0; j < discsNeededToWin; j++) {
+                        if (board[row][column + j].equals(playerToCheck)) {
+                            discsInARowSoFar++;
+                        }
+                    }
+                    if (discsInARowSoFar == discsNeededToWin) {
+                        // If somebody wins, print the final state of the board, then return the winner
+                        printBoard(board, discsNeededToWin);
+                        return playerToCheck;
+                    }
+
+                    // Check verticals for wins
+                    discsInARowSoFar = 0;
+                    for (int j = 0; j < discsNeededToWin; j++) {
+                        if (board[row + j][column].equals(playerToCheck)) {
+                            discsInARowSoFar++;
+                        }
+                    }
+                    if (discsInARowSoFar == discsNeededToWin) {
+                        // If somebody wins, print the final state of the board, then return the winner
+                        printBoard(board, discsNeededToWin);
+                        return playerToCheck;
+                    }
+
+                    // Check left diagonals for wins
+                    discsInARowSoFar = 0;
+                    for (int j = 0; j < discsNeededToWin; j++) {
+                        if (board[row + j][column + j].equals(playerToCheck)) {
+                            discsInARowSoFar++;
+                        }
+                    }
+                    if (discsInARowSoFar == discsNeededToWin) {
+                        // If somebody wins, print the final state of the board, then return the winner
+                        printBoard(board, discsNeededToWin);
+                        return playerToCheck;
+                    }
+
+                    // Check right diagonals for wins
+                    discsInARowSoFar = 0;
+                    for (int j = 0; j < discsNeededToWin; j++) {
+                        if (board[row - j][column - j].equals(playerToCheck)) {
+                            discsInARowSoFar++;
+                        }
+                    }
+                    if (discsInARowSoFar == discsNeededToWin) {
+                        // If somebody wins, print the final state of the board, then return the winner
+                        printBoard(board, discsNeededToWin);
+                        return playerToCheck;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    continue; // LOLOL
+                } // end try/catch
+            } // end checks on respective columns in rows of board
+        } // end checks on rows of board
+    }
+} // end class Ai
